d0077b254ad27f169cd1c413f0fc30a363d0f41d
Nimira43 <n.rabone72@aol.com>
2024-10-26 04:26:06 +0100

    Created Product entity

--- obj/Debug/net8.0/API.AssemblyInfo.cs
+++ obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+816af6da7f4b2b923637e754c26e36e3607d8162")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ded69893214939711a35c3a517df156ed81282d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
