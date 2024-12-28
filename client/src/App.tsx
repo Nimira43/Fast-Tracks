b419e04a0dd9a7770b9767cf96fd435520bb8a96
Nimira43 <n.rabone72@aol.com>
2024-12-28 01:26:50 +0000

    Setting up the client to fetch data and display it - however before connecting this to the .Net backend will need to configure CORS and making the client run on HTTPS.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,28 @@
-const App = () => {
+import { useEffect, useState } from 'react'
+
+function App() {
+  const [products, setProducts] = useState<{name: string, price: number}[]>([])
+
+  useEffect(() => {
+    fetch('https://localhost:5001/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, [])
+    
+  const addProduct = () => {
+    setProducts(prevState => [...prevState, {name: 'product' + (prevState.length + 1), 
+      price: (prevState.length * 100) + 100}])
+  }
+
   return (
     <div>
-      <h1>Fast Tracks</h1>
+      <h1 style={{color: 'orangered'}}>Fast Tracks</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>{item.name} - {item.price}</li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add Product</button>
     </div>
   )
 }
