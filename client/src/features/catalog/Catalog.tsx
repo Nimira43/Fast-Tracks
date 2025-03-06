58831b4e0265fb88f4f7e5905579f37d63081c8b
Nimira43 <n.rabone72@aol.com>
2025-03-06 20:19:23 +0000

    Destructuring

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -5,15 +5,15 @@ type Props = {
   addProduct: () => void
 }
 
-export default function Catalog(props: Props) {
+export default function Catalog({ products, addProduct }: Props) {
   return (
     <>
       <ul>
-        {props.products.map(item => (
+        {products.map(item => (
           <li key={item.id}>{item.name} - {item.price}</li>
         ))}
       </ul>
-      <button onClick={props.addProduct}>Add Product</button>
+      <button onClick={addProduct}>Add Product</button>
     </>
   )
 }
