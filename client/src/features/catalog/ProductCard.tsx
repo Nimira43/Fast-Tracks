ac4c501e809a0db3bce4ec37a48b0920708deb3f
Nimira43 <n.rabone72@aol.com>
2025-03-07 00:57:12 +0000

    PRoductCard completed for the moment.

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardMedia, Typography } from '@mui/material'
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material'
 import { Product } from '../../app/models/products'
 
 type Props = {
@@ -18,12 +18,24 @@ export default function ProductCard({product}: Props) {
       <CardContent>
         <Typography
           gutterBottom
-          sx={{ color: 'seconardy.main' }}
+          sx={{ textTransform: 'uppercase' }}
           variant='subtitle2'
         >
           <span>{product.name}</span>
         </Typography>  
+        <Typography
+          variant='h6'
+          sx={{ color: 'secondary.main'}}
+        >
+          <span>£{(product.price / 100).toFixed(2)}</span>
+        </Typography>
       </CardContent>
+      <CardActions
+        sx={{ justifyContent: 'space-between' }}
+      >
+        <Button>Add To Cart</Button>
+        <Button>Review</Button>
+      </CardActions>
     </Card>
   )
 }
