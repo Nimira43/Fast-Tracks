270332d6f82e54816bcf0d0756bbb96915fea399
Nimira43 <n.rabone72@aol.com>
2025-03-19 01:50:44 +0000

    Added a key to the ProductCard

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -12,7 +12,10 @@ export default function ProductList({products}: Props) {
       sx={{display: 'flex', flexWrap: 'wrap', gap: 3, justifyContent: 'center'}}
     >
       {products.map(product => (
-        <ProductCard product={product} />
+        <ProductCard
+          key={product.id}
+          product={product}
+        />
       ))}
     </Box>
   )
