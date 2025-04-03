253b8974befc2b06f1a8885c9283dab1ab19e3c7
Nimira43 <n.rabone72@aol.com>
2025-04-03 21:44:07 +0100

    Create theme variable and set it to dark. Applide it to Material UI's ThemeProvider

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from 'react'
 import { Product } from '../models/products'
 import Catalog from '../../features/catalog/Catalog'
-import { Container } from '@mui/material'
+import { Container, createTheme, ThemeProvider } from '@mui/material'
 import NavBar from './NavBar'
 
 function App() {
   const [products, setProducts] = useState<Product[]>([])
+
+  const theme = createTheme({
+    palette: {
+      mode: 'dark'
+    }
+  })
 
   useEffect(() => {
     fetch('https://localhost:5001/api/products')
@@ -14,12 +20,12 @@ function App() {
   }, [])
     
   return (
-    <>
+    <ThemeProvider theme={theme} >
       <NavBar />
       <Container maxWidth='xl' sx={{mt: 15}}>
         <Catalog products={products} />
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
