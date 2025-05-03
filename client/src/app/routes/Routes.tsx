c2488c65e595981e6984cee102f4a89ed60bcd0c
Nimira43 <n.rabone72@aol.com>
2025-05-03 23:21:06 +0100

    Setting up App route and it's children - first off - HomePage

--- client/src/app/routes/Routes.tsx
+++ client/src/app/routes/Routes.tsx
@@ -1,7 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom'
+import App from '../layout/App'
+import HomePage from '../../features/home/HomePage'
 
 export const router = createBrowserRouter([
   {
-    
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <HomePage />,
+      },
+      
+    ]
   }
 ])
