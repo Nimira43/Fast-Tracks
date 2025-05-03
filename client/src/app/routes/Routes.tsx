0e7aa21461a81465b0c5b9ba692068836424a1d0
Nimira43 <n.rabone72@aol.com>
2025-05-03 23:24:38 +0100

    Routes for Catalog and ProductDetails

--- client/src/app/routes/Routes.tsx
+++ client/src/app/routes/Routes.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from '../layout/App'
 import HomePage from '../../features/home/HomePage'
+import Catalog from '../../features/catalog/Catalog'
+import ProductDetails from '../../features/catalog/ProductDetails'
 
 export const router = createBrowserRouter([
   {
@@ -11,7 +13,23 @@ export const router = createBrowserRouter([
         path: '',
         element: <HomePage />,
       },
-      
+      {
+        path: '/catalog',
+        element: <Catalog />,
+      },
+      {
+        path: '/catalog/:id',
+        element: <ProductDetails />,
+      },
+      {
+        path: '',
+        element: <HomePage />,
+      },
+      {
+        path: '',
+        element: <HomePage />,
+      },
+
     ]
   }
 ])
