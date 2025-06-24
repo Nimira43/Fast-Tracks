0e3cd8d5f9f78c3dc8be646b6081a0622eceeef3
Nimira43 <n.rabone72@aol.com>
2025-06-24 20:52:30 +0100

    Setting type

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,8 +1,10 @@
 import { useParams } from 'react-router-dom'
+import { Product } from '../../app/models/products'
+import { useState } from 'react'
 
 export default function ProductDetails() {
   const { id } = useParams()
-  const [product, setProduct] = useState()
+  const [product, setProduct] = useState  <Product | null>(null)
   return (
     <div>ProductDetails</div>
   )
