aed901e813d4bfab3cd75cdab1ad469d4a29b73f
Nimira43 <n.rabone72@aol.com>
2025-06-24 20:54:38 +0100

    Importing useEffect and fetching products

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,10 +1,15 @@
 import { useParams } from 'react-router-dom'
 import { Product } from '../../app/models/products'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function ProductDetails() {
   const { id } = useParams()
   const [product, setProduct] = useState  <Product | null>(null)
+
+  useEffect(() => {
+    fetch(`https://localhost:5001/api/products/${id}`)
+  }, [])
+
   return (
     <div>ProductDetails</div>
   )
