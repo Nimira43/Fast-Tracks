422014e74faa580e80c2e7e7c59eb44e30a9c902
Nimira43 <n.rabone72@aol.com>
2025-06-24 21:34:28 +0100

    Handling possible future warnings - commented out for now as there are no warnings showing.

--- client/src/app/routes/Routes.tsx
+++ client/src/app/routes/Routes.tsx
@@ -33,4 +33,16 @@ export const router = createBrowserRouter([
       },
     ]
   }
-])
+], 
+
+// {
+//   future: {
+//     v7_relativeSplatPath: true,
+//     v7_fetcherPersist: true,
+//     v7_normalizeFormMethod: true,
+//     v7_partialHydration: true,
+//     v7_skipActionErrorRevalidation: true,
+//   }
+// }
+
+)
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './app/layout/styles.css'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './app/routes/routes'
+import { router } from './app/routes/Routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider 
+      router={router}
+      // future={{v7_startTransition: true}}  
+    />
   </StrictMode>,
 )
