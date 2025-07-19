5981e0e1a5729e949ace2dbf9e7b875e4ef56886
Nimira43 <n.rabone72@aol.com>
2025-07-19 03:32:58 +0100

    My cat Lenny's just been helping out!!!

--- client/src/features/contact/counterReducer.ts
+++ client/src/features/contact/counterReducer.ts
@@ -2,3 +2,10 @@ export type CounterState = {
   data: number
 }
 
+const initialState : CounterState = {
+  data: 42
+}
+
+export default function counterReducer(state = initialState) {
+  return statecx       cvv \-------------------------z\as56ppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp0-t     gggggggg```````1ewwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwq1111111111111111111111111111111111111111111111111111111111111111111111111w21111111111111111309                        
+}
