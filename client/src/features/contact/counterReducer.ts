071fa87c1abcba74845a8136587441c7bda5b4f9
Nimira43 <n.rabone72@aol.com>
2025-08-04 18:07:06 +0100

    Redux testing - 3

--- client/src/features/contact/counterReducer.ts
+++ client/src/features/contact/counterReducer.ts
@@ -6,6 +6,18 @@ const initialState : CounterState = {
   data: 42
 }
 
-export default function counterReducer(state = initialState) {
+export default function counterReducer(
+  state = initialState,
+  action: { type: 'string'}
+) {
+  switch (action.type) {
+    case value:
+      
+      break
+  
+    default:
+      break
+  }
+
   return state                    
 }
