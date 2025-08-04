059c74810c4309d7c1211feb2f9b22fbcd5d8ea7
Nimira43 <n.rabone72@aol.com>
2025-08-04 17:53:54 +0100

    Brought Provider into main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './app/layout/styles.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './app/routes/Routes'
+import { Provider } from 'react-redux'
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider 
+    <Provider store={store}>
+      <RouterProvider 
       router={router}
       // future={{v7_startTransition: true}}  
     />
+    </Provider>
+    
   </StrictMode>,
 )
