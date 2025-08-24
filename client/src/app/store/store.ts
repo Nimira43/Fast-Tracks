cd19d0898608f552ffa42a03b02aee0d8202c2bc
Nimira43 <n.rabone72@aol.com>
2025-08-24 20:18:05 +0100

    Created new store in store.ts

--- client/src/app/store/store.ts
+++ client/src/app/store/store.ts
@@ -1,6 +1,15 @@
-import { legacy_createStore } from '@reduxjs/toolkit'
-import counterReducer from '../../features/contact/counterReducer'
+import { configureStore } from '@reduxjs/toolkit'
+import { counterSlice } from '../../features/contact/counterReducer'
 
-export function configureTheStore() {
-  return legacy_createStore(counterReducer)
-}
+// import { legacy_createStore } from '@reduxjs/toolkit'
+// import counterReducer from '../../features/contact/counterReducer'
+
+// export function configureTheStore() {
+//   return legacy_createStore(counterReducer)
+// }
+
+export const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer
+  }
+})
