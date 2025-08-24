56abe556ed4b068d48d319b2edd660c4182756f3
Nimira43 <n.rabone72@aol.com>
2025-08-24 20:41:41 +0100

    Updating store - 2

--- client/src/app/store/store.ts
+++ client/src/app/store/store.ts
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { counterSlice } from '../../features/contact/counterReducer'
-import { UseDispatch } from 'react-redux'
-import { RootState } from '@reduxjs/toolkit/query'
+import { useDispatch, useSelector } from 'react-redux'
 
 export const store = configureStore({
   reducer: {
@@ -9,5 +8,9 @@ export const store = configureStore({
   }
 })
 
-export type useAppDispatch = useDispatch.withTypes<AppDispatch>
-export type useAppSelector = useSelector.withTypes<RootState>
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>
+
+export const useAppSelector = useSelector.withTypes<RootState>
