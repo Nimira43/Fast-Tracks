c9754e92758a13889fb6734003347b9fa10f92c1
Nimira43 <n.rabone72@aol.com>
2025-08-24 22:09:10 +0100

    Cleared out code inside Catalog function

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -4,13 +4,7 @@ import ProductList from './ProductList'
 
 export default function Catalog() {
   
-  const [products, setProducts] = useState<Product[]>([])
-  
-  useEffect(() => {
-      fetch('https://localhost:5001/api/products')
-        .then(response => response.json())
-        .then(data => setProducts(data))
-    }, []) 
+ 
   
   return (
     <>
