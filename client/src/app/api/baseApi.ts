37b6faf370b0b90edcb19d135ccd3c24de9278c8
Nimira43 <n.rabone72@aol.com>
2025-09-20 14:25:38 +0100

    baseQueryWithErrorHandling - 1

--- client/src/app/api/baseApi.ts
+++ client/src/app/api/baseApi.ts
@@ -1,7 +1,9 @@
-import { fetchBaseQuery } from '@reduxjs/toolkit/query'
+import { FetchArgs, fetchBaseQuery } from '@reduxjs/toolkit/query'
 
 const customBaseQuery = fetchBaseQuery({
   baseUrl: 'https://localhost:5001/api'
 })
 
 const sleep = () => new Promise(resolve => setTimeout(resolve, 1000))
+
+export const baseQueryWithErrorHandling = async (arg: string | FetchArgs)
