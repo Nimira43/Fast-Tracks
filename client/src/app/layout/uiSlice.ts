8e76efd4c1dc073035218dc5bc756471332a5e69
Nimira43 <n.rabone72@aol.com>
2025-09-30 16:33:55 +0100

    InitialState and reducers

--- client/src/app/layout/uiSlice.ts
+++ client/src/app/layout/uiSlice.ts
@@ -1,3 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-export const uiSlice = createSlice
+export const uiSlice = createSlice({
+  name: 'ui',
+  initialState: {
+    isLoading: false
+  },
+  reducers: {
+    startLoading: (state) => {
+      state.isLoading = true
+    }
+  }
+})
