b27a1e3c8ab435f5da7d98fe271829a72022881f
Nimira43 <n.rabone72@aol.com>
2025-11-03 20:06:23 +0000

    BuggyController -1

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,8 +1,11 @@
 using System;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
 public class BuggyController : BaseApiController
 {
 
+  [HttpGet("not-found")]
+  public IActionResult
 }
