480ab4e4f79d2c2c6dcc5d36b844ecb5d37261a0
Nimira43 <n.rabone72@aol.com>
2025-11-18 21:19:01 +0000

    Added ExceptionMiddleware to Program

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Middleware;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,9 +10,11 @@ builder.Services.AddDbContext<StoreContext>(opt =>
   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddTransient<ExceptionMiddleware>();
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(opt => 
 {
   opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
