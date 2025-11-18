2d4b6704fdedd95b1b5448c80fdb83f1930b8899
Nimira43 <n.rabone72@aol.com>
2025-11-18 14:11:53 +0000

    params

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -3,7 +3,8 @@ using System;
 namespace API.Middleware;
 
 public class ExceptionMiddleware(
-
+  IHostEnvironment env, 
+  ILogger<ExceptionMiddleware> logger
 ) : IMiddleware
 {
   public Task InvokeAsync(HttpContext context, RequestDelegate next)
