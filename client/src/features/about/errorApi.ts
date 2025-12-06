783f3d14ce43b7dd4335c444796fdf7f76f2d85b
Nimira43 <n.rabone72@aol.com>
2025-12-06 12:14:24 +0000

    errorApi - 3

--- client/src/features/about/errorApi.ts
+++ client/src/features/about/errorApi.ts
@@ -7,6 +7,18 @@ export const errorApi = createApi({
   endpoints: (builder) => ({
     get400Error: builder.query<void, void>({
       query: () => ({url: 'buggy/bad-request'})
+    }),
+    get401Error: builder.query<void, void>({
+      query: () => ({url: 'buggy/unauthorized'})
+    }),
+    get404Error: builder.query<void, void>({
+      query: () => ({url: 'buggy/not-found'})
+    }),
+    get500Error: builder.query<void, void>({
+      query: () => ({url: 'buggy/server-error'})
+    }),
+    getValidationError: builder.query<void, void>({
+      query: () => ({url: 'buggy/validation-error'})
     })
   })
 })
