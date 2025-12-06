54e40676e763fa3ea5eb6a00809dd6e3d3b379b6
Nimira43 <n.rabone72@aol.com>
2025-12-06 12:09:02 +0000

    errorApi - 1

--- client/src/features/about/errorApi.ts
+++ client/src/features/about/errorApi.ts
@@ -1,3 +1,7 @@
 import { createApi } from '@reduxjs/toolkit/query/react'
+import { baseQueryWithErrorHandling } from '../../app/api/baseApi'
 
-export const errorApi = createApi
+export const errorApi = createApi({
+  reducerPath: 'errorApi',
+  baseQuery: baseQueryWithErrorHandling
+})
