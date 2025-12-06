30313c1da17227e3518cce6e45a0b97157885433
Nimira43 <n.rabone72@aol.com>
2025-12-06 12:21:11 +0000

    Updated configureStore

--- client/src/app/store/store.ts
+++ client/src/app/store/store.ts
@@ -13,7 +13,10 @@ export const store = configureStore({
     ui: uiSlice.reducer
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(catalogApi.middleware)
+    getDefaultMiddleware().concat(
+      catalogApi.middleware,
+      errorApi.middleware
+    )
 })
 
 export type RootState = ReturnType<typeof store.getState>
