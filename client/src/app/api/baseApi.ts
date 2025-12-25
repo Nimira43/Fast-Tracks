b6691c8b32362d4bbf345ad8ee7ecda153c94651
Nimira43 <n.rabone72@aol.com>
2025-12-25 12:51:06 +0000

    ....and into the 401

--- client/src/app/api/baseApi.ts
+++ client/src/app/api/baseApi.ts
@@ -30,7 +30,7 @@ export const baseQueryWithErrorHandling = async (
         toast.error(responseData as string)
         break
       case 401:
-        toast.error(data.title)
+        toast.error(reponseData.title)
         break
       default:
         break
