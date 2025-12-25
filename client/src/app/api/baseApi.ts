53d9dbdf70bad7b566e31ec74a4b3e339d25fb23
Nimira43 <n.rabone72@aol.com>
2025-12-25 15:38:27 +0000

    404 case

--- client/src/app/api/baseApi.ts
+++ client/src/app/api/baseApi.ts
@@ -40,6 +40,9 @@ export const baseQueryWithErrorHandling = async (
       case 401:
         if (typeof responseData === 'object' && 'title' in responseData) toast.error(responseData.title)
         break
+      case 404:
+        if (typeof responseData === 'object' && 'title' in responseData) toast.error(responseData.title)
+        break
       default:
         break
     }
