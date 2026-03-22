645d65914b6a884e5de402a220bc438772474398
Nimira43 <n.rabone72@aol.com>
2026-03-22 19:59:12 +0000

    Using extension methods.

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,24 +16,11 @@ public class BasketController(StoreContext context) : BaseApiController
 
     if (basket == null) return NoContent();
 
-    return new BasketDto
-    {
-      BasketId = basket.BasketId,
-      Items = basket.Items.Select(x => new BasketItemDto
-      {
-        ProductId = x.ProductId,
-        Name = x.Product.Name,
-        Price = x.Product.Price,
-        Brand = x.Product.Brand,
-        Type = x.Product.Type,
-        PictureUrl = x.Product.PictureUrl,
-        Quantity = x.Quantity
-      }).ToList()
-    };
+    return basket.ToDto();
   }
 
   [HttpPost]
-  public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
+  public async Task<ActionResult<BasketDto>> AddItemToBasket(int productId, int quantity)
   {
     var basket = await RetrieveBasket();
 
@@ -46,7 +34,7 @@ public class BasketController(StoreContext context) : BaseApiController
 
     var result = await context.SaveChangesAsync() > 0;
 
-    if (result) return CreatedAtAction(nameof(GetBasket), basket);
+    if (result) return CreatedAtAction(nameof(GetBasket), basket.ToDto());
 
     return BadRequest("Problem updating basket");
   }
