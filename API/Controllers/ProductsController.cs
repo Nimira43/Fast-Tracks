a89dea9f52cdbcdfa43c482050d17507459596a9
Nimira43 <n.rabone72@aol.com>
2026-05-26 17:49:55 +0100

    Search functionality.

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,26 +6,30 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class ProductsController(StoreContext context) : BaseApiController
+  public class ProductsController(StoreContext context) : BaseApiController
+  {
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProducts(
+      string? orderBy,
+      string? searchTerm
+    )
     {
-        [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy)
-        {
-            var query = context.Products
-                .Sort(orderBy)
-                .AsQueryable();
+      var query = context.Products
+        .Sort(orderBy)
+        .Search(searchTerm)
+        .AsQueryable();
 
-            return await query.ToListAsync();
-        }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id) 
-        {
-            var product = await context.Products.FindAsync(id);
-
-            if (product == null) return NotFound();
-
-            return product;
-        }
+      return await query.ToListAsync();
     }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+      var product = await context.Products.FindAsync(id);
+
+      if (product == null) return NotFound();
+
+      return product;
+    }
+  }
 }
