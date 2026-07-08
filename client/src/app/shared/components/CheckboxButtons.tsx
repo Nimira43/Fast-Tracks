2c9cc971ee3ee4664842a5f674d78a607c402386
Nimira43 <n.rabone72@aol.com>
2026-07-08 12:02:58 +0100

    CheckboxButtons component.

---
+++ client/src/app/shared/components/CheckboxButtons.tsx
@@ -0,0 +1,53 @@
+import { Checkbox, FormControlLabel, FormGroup } from '@mui/material'
+import { useEffect, useState } from 'react'
+
+type Props = {
+  items: string[]
+  checked: string[]
+  onChange: (items: string[]) => void
+}
+
+export default function CheckboxButtons({items, checked, onChange}: Props) {
+  const [checkedItems, setCheckedItems] = useState(checked)
+
+  useEffect(() => {
+    setCheckedItems(checked)
+  }, [checked])
+  
+  const handleToggle = (value: string) => {
+    const updatedChecked = checkedItems?.includes(value)
+      ? checkedItems.filter(item => item !== value)
+      : [...checkedItems, value]
+    
+    setCheckedItems(updatedChecked)
+    onChange(updatedChecked)
+  }
+  
+  return (
+    <FormGroup>
+      {items.map(item => (
+        <FormControlLabel
+          key={item}
+          control={
+            <Checkbox
+              checked={checkedItems.includes(item)}
+              onClick={() => handleToggle(item)}
+              sx={{
+                py: 0.7,
+                fontSize: 40,
+                color: 'orange',
+                '&.Mui-checked': {
+                  color: 'orange',
+                },
+                '&.Mui-focusVisible': {
+                  outline: 'none',
+                },
+              }}
+            />
+          }
+          label={item}
+        />
+      ))}
+    </FormGroup>
+  )
+}
